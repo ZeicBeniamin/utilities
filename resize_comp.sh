b27aeea6aa0387f4458c3352afc0f798fb3ea0d2
ZeicBeniamin <beniamin22052000@gmail.com>
2021-06-08 12:19:29 +0300

    Added pdf creation functionality to resize_comp.sh

--- resize_comp.sh
+++ resize_comp.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
-# This script resizes all the png or jpg files in the current folder.
+# This script creates a pdf out of the images in the current folder (jpg or png).
+#
+# PREREQUESITES:
+# 	imagemagick
+#	ffmpeg
+#
+# SCRIPT ACTIONS:
+# The images are first resized to a user-specified width and compressed using
+# a compression factor of 70. The compressed and resized images are moved in a
+# separate folder. The original images are also relocated in a folder.
+# All the processed files are then converted into a pdf file, using JPEG
+# compression with a quality factor of 50.
 #
 # The files must follow a specific naming convention and should be indexed
 # by a number. The name of a file must have the following format:
 #
 #		{name}_{number}.{extension}
 #
-# where {name} is a generic name for all the files to be processed,
-# {number} is the number that indexes each of the files and {extension} is
-# the file extension, that is either .png or .jpg"
-#
-# A new folder will be created, that will store all the resized and compressed
-# files.
+# where {name} is a generic name for all the files to be processed, {number} is
+# the number that indexes each of the files and {extension} is the file extension,
+# that is either .png or .jpg".
 
 
 Help() {
@@ -51,13 +59,16 @@ ext=$4
 # Width of the resized file
 width=$5
 # Name of the temporary file
-temp_name="temp_file.jpg"
+temp_name="temp_file.${ext}"
+# Array of file names to be stacked in pdf file
+arr=""
 
 # Print the data received as input for control purposes
 echo "Generic file name is ${file_name}"
 echo "Start and end indexes are ${start_it}, ${end_it}"
 echo "File extension is ${ext}"
 echo "Image width is ${width}"
+echo
 
 # Create a folder for the resized and compressed images, if it does not already exist
 folder_name="resize_comp_${width}"
@@ -65,6 +76,13 @@ if [ ! -d ${folder_name} ];
 then
 	echo "Created directory ${folder_name}."
 	mkdir $folder_name
+fi
+# Create a folder for the original images
+orig_folder="originals"
+if [ ! -d ${orig_folder} ]; 
+then
+	echo "Created directory ${folder_name}."
+	mkdir $orig_folder
 fi
 
 iter=$start_it
@@ -92,11 +110,31 @@ do
 		-i $temp_name \
 		-compression_level 70 \
 		$out_fil
-		echo "${fil} resized and compressed"
+		echo "${fil} resized and compressed."
+		# Move the file after processing
+		mv ${fil} ${orig_folder}"/"${fil}
+		# Store the name of the file in array for later processing
+		arr+=" $out_fil"
 	else
-		echo "${fil} not found"
+		echo "${fil} not found."
 	fi
 	# Pass to the next file
 	((iter++))
 done
 
+# Merge the images into a single pdf file.
+convert $arr -quality 50 -compress JPEG "${file_name}.pdf"
+
+if [ $? -eq 0 ]; then
+	echo
+	echo "PDF created succesfuly"
+else
+	echo
+	echo "PDF not created"
+fi
+
+if [ -f ${temp_name} ];
+then
+	rm ${temp_name}
+	echo "Deleted temporary file ${temp_name}."
+fi
